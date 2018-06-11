115521aa99217c8c867c4f132a7b2d1c3fca5c4c
Aymeric Gaillard <aymeric.gaillard@gmail.com>
2018-06-11 11:19:27 +0200

    add every class for Piece (abstract and inherited) ¯\_(ツ)_/¯

---
+++ src/model/Reine.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+/**
+ *
+ * @author Aymeric
+ */
+public class Reine extends AbstractPiece {
+
+    public Reine() {
+        super();
+    }
+    
+}
